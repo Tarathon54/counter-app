7fe99f174dc178c38f8bfa96a61628fc8acdbdae
Darren <darren.w45@yahoo.com>
2021-08-24 11:26:47 +0100

    amnded test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+    
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('starts with a count of 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 0');
+  });
+});
+
+// src/App.test.js
+    
+describe('App component', () => {
+  ...
+  
+  it('increments count by 1 when the increment button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 1');
+
+    it('decrements count by 1 when the decrement button is clicked', () => {
+      const wrapper = shallow(<App />);
+      const decrementBtn = wrapper.find('button.decrement');
+      decrementBtn.simulate('click');
+      const text = wrapper.find('p').text();
+      expect(text).toEqual('Count: -1');
+
+  });
 });
